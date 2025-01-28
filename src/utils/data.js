7cc8995156f7a045b7d4aa8859627df33dadd769
predrag jandric <predrag.jandric.bg@gmail.com>
2025-01-28 23:52:34 +0100

    will ADD: data to be used for map methods across the app

---
+++ src/utils/data.js
@@ -0,0 +1,66 @@
+// DATA FOR MAP METHODS ACROSS THE APP
+// MODIFY MAP DATA HERE AND NOWHERE ELSE.
+
+export const navLinks = [
+  { href: "staucimo", label: "Šta učimo" },
+  { href: "vizija", label: "Vizija" },
+  { href: "strategija", label: "Strategija" },
+  { href: "cestaPitanja", label: "Česta Pitanja" },
+  { href: "kontakt", label: "Kontakt" },
+];
+
+export const faqMap = [
+  {
+    question: "Da li mi je potrebno predznanje iz IT oblasti?",
+    answer:
+      "Ne uopšte. U programiranju se uči način razmišljanja, stoga časove prilagođavam vašem nivou, bilo da počinjemo od nule ili da već imate neko znanje, napredak je zagarantovan. Počinjemo od osnova HTML i CSS, zatim ubrzo prelazimo na JavaScript pa potom React i Tailwind kombinaciju koji je najpopularniji i zastupljeniji front-end stack. Svi ovi jezici se samo nadograđuju jedan na drugi.",
+  },
+
+  {
+    question: "Šta zapravo učimo na časovima?",
+    answer:
+      "Časovi se sastoje od kombinacije više aktivnosti: teorije, primene znanja, diskusije bilo kakvih nedoumica i domaćeg rada prilagodjenog vama, kako bi se sigurno savladala tematika. Posle toga zajedno ulazimo u svet open source projekata koji idealno simuliraju rad u realnom svetu.",
+  },
+  {
+    question: "Koliko će mi trebati vremena da mogu sam da napravim sajt?",
+    answer:
+      "Za 3 meseca posvećenog rada, totalni početnik može napraviti statički web sajt sa nekim osnovnim interakcijama i postaviti ga uživo na internet. Uz moju pomoć, ovakav websajt vam može služiti kao digitalna vizit karta i može vas predstavljati u bilo kojoj sferi života, čak i van IT-ja, i pomoći vam da se istaknete od drugih. Ovakav projekat se ne naplaćuje dodatno, već ga zajedno pravimo kroz časove učeći, uz moje smernice i pomoć ako negde zapne.",
+  },
+  {
+    question: "Kakve ćemo projekte praviti?",
+    answer:
+      "Počećemo od jednostavnih zadataka gde dobijate gotov dizajn koji treba da napišete u kodu, zatim prelazimo na jednostavne logičke zadatke poput to-do aplikacija. Nakon toga radimo na naprednim logičkim aplikacijama, pa sve do velikih projekata na kojima radi više različitih developera. Brzinu napretka prilagođavamo tako da prelazimo na sledeći nivo tek kada smo sigurni u svoje mogućnosti.",
+  },
+  {
+    question: "Da li je front-end pravac programiranja dobar izbor?",
+    answer:
+      "Naravno. Programiranje pruža širok spektar mogućnosti. Možete da radite za sebe kao frilenser na globalnom ili lokalnom tržištu, da pravite web sajtove iz hobija za sebe, svoje prijatelje i open-source zajednicu, ili da radite za neku od mnogobrojnih firmi, bilo od kuće (remote) ili iz kancelarije. Najbolje od svega je da koncepti programiranja koje ste stekli ostaju sa vama zauvek i imaju primenu u mnogim drugim sferama života, jer je programiranje, u svojoj srži, veština rešavanja problema.",
+  },
+  {
+    question: "Kada vremenski možemo da održavamo časove?",
+    answer:
+      "Kada vama odgovara, sve je stvar dogovora. Nije nikakav problem ni pomeriti zakazan termin tako da se uklapa u vaš novonastali raspored posla, porodice ili drugih obaveza. Sve teme koje budemo pokrili uvek ćete moći da se vratite i ponovite uz obezbeđene resurse za vežbanje.",
+  },
+  {
+    question: "Koja je cena časova?",
+    answer:
+      "Cena je prilagođena vama, što znači da je niska kada počinjemo od jednostavnih stvari, a blago raste kako prelazimo na naprednije teme. Ako se odlučite za paket časova koji plaćate odjednom, dobijate besplatne časove kao i mnoge druge bonuse.",
+  },
+  {
+    question: "Zašto da izaberem baš vas?",
+    answer:
+      "Posvećen sam pružanju vrednosti klijentu za uloženi novac, svojom sveobuhvatnom metodom rada. U svom prethodnom poslu sam takođe bio mentor, sa zadatkom da klijente dovedem do rezultata, a ne samo da iznajmim svoje vreme. Nudim podršku i van časova, ako naiđete na neki problem. Dajem domaće zadatke kroz koje usavršavate stečena znanja i uvodim vas u open-source zajednicu, gde ćete imati priliku da sarađujete sa programerima širom sveta, praveći aplikacije. Steći ćete iskustvo u menadžmentu aplikacije, online sastancima, prezentacijama, timskom donošenju odluka, komunikaciji sa dizajnerima i stejkholderima. Ovo je najnapredniji nivo, na volonterskoj bazi, gde ni jedna strana ne zarađuje od sajta napravljenog na ovaj način.",
+  },
+];
+
+export const staucimoMap = [
+  { name: "HTML", staucimoImageUrl: "/assets/staucimo/html.png" },
+  { name: "CSS", staucimoImageUrl: "/assets/staucimo/css.png" },
+  { name: "JavaScript", staucimoImageUrl: "/assets/staucimo/js.png" },
+  { name: "Git / Github", staucimoImageUrl: "/assets/staucimo/git.png" },
+  { name: "React.js", staucimoImageUrl: "/assets/staucimo/react.png" },
+  {
+    name: "Redux",
+    staucimoImageUrl: "/assets/staucimo/redux-toolkit.png",
+  },
+];
